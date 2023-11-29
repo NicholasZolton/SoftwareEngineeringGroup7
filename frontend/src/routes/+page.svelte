cd66bd748559eeee58f3493ea0d7bc94429bd2b9
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-11-28 21:26:14 -0600

    Remove unused file and update page content

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang='ts'>
+	import { user } from "$lib/user";
+</script>
+
+<h1>Welcome to SvelteKit! The user is {$user}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+
+</style>
